002a5d6c5ea7fca55f9f9315b74f0e6aae1e1503
swtpumpkin <swtpumpkin90@gmail.com>
2017-08-02 17:06:21 +0900

    Update Pyramid.js file. I revised comment.

--- Exercise/Pyramid.js
+++ Exercise/Pyramid.js
@@ -4,11 +4,11 @@ var result = '';
 
 for(var num = 0; num<inputHeight; num++){ // 0부터 피라미드 높이까지 도는 for 문.
   for(var empty = 1; empty<inputHeight-num; empty++){ //빈칸 입력하는 for 문.
-    result +=' ';
+    result +=' '; //띄어쓰기 입력하는 코드.
   }
   for(var star = num; star>=-num; star--){ //피라미드 입력하는 for문.
-    result += '*';
+    result += '*'; //별 입력하는 코드.
   }
-  result +='\n'; //개행하는 함수.
+  result +='\n'; //개행하는 코드.
 }
 console.log(result); //결과를 출력.
