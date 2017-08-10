5f87916aba48ed8e8f02e83abb59662d5af9e747
JeongwonKim <swtpumpkin90@gmail.com>
2017-08-10 22:22:22 +0900

    Create compareNumber.js file.

---
+++ Exercise/compareNumber.js
@@ -0,0 +1,14 @@
+//두 수 중에서 큰 숫자를 출력한다.
+function max (a, b) {
+  //숫자형인지 검색한다.
+  if(typeof a && typeof b === 'number'){
+    //a가 클경우 a를 반환한다.
+    if(a>b){
+      return a;
+    }
+    //b가 클경우 b를 반환한다.
+    return b;
+  }
+}
+console.log(max(1,5));
+console.log(max(7,3));
