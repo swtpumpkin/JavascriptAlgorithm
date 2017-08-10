a9bc4bdbf988e5f2936d71b2c65c6b72d1d5ad0c
JeongwonKim <swtpumpkin90@gmail.com>
2017-08-10 21:51:51 +0900

    Create secondStack.js file.

---
+++ Exercise/secondStack.js
@@ -0,0 +1,25 @@
+(function printNow() {
+  var today = new Date();
+  var dayNames= ['(일요일)','(월요일)','(화요일)','(수요일)','(목요일)','(금요일)','(토요일)'];
+  var day = dayNames[today.getDay()];
+  var year = today.getFullYear(),
+      month = today.getMonth() + 1,
+      date = today.getDate(),
+      hour = today.getHours(),
+      minute = today.getMinutes(), 
+      second = today.getSeconds(),
+      ampm = hour >= 12? 'PM' : 'AM';
+
+  //12시간제로 변경
+  hour = hour % 12;
+  hour = hour ? hour : 12;
+
+  //10미만인 분과 초를 2자리로 변경
+  minute = minute < 10? '0' + minute : minute;
+  second = second < 10? '0' + second : second;
+
+  //현재 시간
+  var now = year + '년' + month + '월' + date + '일' + day + ' ' + hour + ':' + minute + ':' + second + ' ' + ampm;
+  console.log(now);
+  setTimeout(printNow, 1000);
+}());
