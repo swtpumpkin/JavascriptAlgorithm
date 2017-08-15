d2dcd730bd4f3b22e35b0c4ae21dd4f78d4a4df1
JeongwonKim <swtpumpkin90@gmail.com>
2017-08-15 22:05:30 +0900

    Update findMinDistance.js file. I added comment and deleted variable line.

--- Exercise/findMinDistance.js
+++ Exercise/findMinDistance.js
@@ -1,13 +1,16 @@
 //최단 거리 1차원 점의 쌍 구하기 (DAUM)
 function findMinDistance(array){
-  var distance = 0;
+  //반환할 배열의 빈 문자열 생성
   var arr = [];
+  //0부터 Array.length의 길이만큼 for loop 생성
   for(var num = 0; num<array.length; num++){
-    distance = array[num+1]-array[num];
-    if(distance === 1){
+    //배열의 숫자 차이가 1인 경우를 찾음
+    if(array[num+1]-array[num] === 1){
+      //빈 배열에 숫자 차이가 1인 배열 쌍을 배열로 추가한다.
       arr.push([array[num],array[num+1]]);
     }
   }
+  //추가된 배열을 출력한다.
   return arr;
 }
 // 1차원 점의 배열
