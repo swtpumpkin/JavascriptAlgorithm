a751851f509a73f92caa59d1a6b7b6cfb2f33e40
JeongwonKim <swtpumpkin90@gmail.com>
2017-08-16 10:39:44 +0900

    Revised palindrome.js file.

--- Exercise/palindrome.js
+++ Exercise/palindrome.js
@@ -6,6 +6,6 @@ function checkPalindrom(str) {
 console.log(checkPalindrom('dad')); // true
 console.log(checkPalindrom('mom')); // true
 console.log(checkPalindrom('palindrom')); // false
-console.log(checkPalindrom('s')); // false
+console.log(checkPalindrom('s')); // true
 console.log(checkPalindrom('abccba')); // true
 console.log(checkPalindrom('abcdba')); // false
