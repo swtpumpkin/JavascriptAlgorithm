bd74e46f1a33a47bd252034cc55664dd78c8cec1
JeongwonKim <swtpumpkin90@gmail.com>
2017-08-16 10:37:15 +0900

    Revise palindrome.js file.

--- Exercise/palindrome.js
+++ Exercise/palindrome.js
@@ -1,7 +1,7 @@
 // 오른쪽 또는 왼쪽으로 읽어도 같은 문자열을 갖는 함수 만들기
 function checkPalindrom(str) {
   // 정방향 또는 역방향을 이용하여 문자열이 같는지 확인한다.
-  if(str === str.split('').reverse('').join('')){
+  if(str === str.split('').reverse().join('')){
     // 문자열이 2개 이상인지 확인한다.
     if(str.length>1){
       return true;
