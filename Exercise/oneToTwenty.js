7a133dafdc0f74a8f6d8ff17481e227acac5d265
JeongwonKim <swtpumpkin90@gmail.com>
2017-08-24 22:07:46 +0900

    Revise oneToTwenty.js file. I revised code & comment.

--- Exercise/oneToTwenty.js
+++ Exercise/oneToTwenty.js
@@ -1,10 +1,8 @@
 //1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 얼마입니까?
-//최대 수를 변수에 담는다.
-var maxNum = Number.MAX_VALUE;
 //반환될 가장 작은 수를 담을 변수를 선언한다.
 var minNum = 0;
-//1부터 가장 큰 수까지 도는 for문을 선언한다.
-for(var num = 1; num<maxNum; num++){
+//1부터 1씩 더해지는 무한루프를 만든다.
+for(var num = 1; ; num++){
   var count = 0;
   //1부터 20까지 나눈 나머지가 0인 수를 찾는 for문을 선언한다.
   for(var innerNum = 1; innerNum <= 20; innerNum++){
