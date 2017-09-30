c37723c33b3f7b10e07ffd382fdacb9ff6d5719e
JeongwonKim <swtpumpkin90@gmail.com>
2017-09-30 09:55:27 +0900

    연산을 빼고 0이면 false 1이면 true를 이용함.

--- Exercise/EvenOrOddFunc.js
+++ Exercise/EvenOrOddFunc.js
@@ -1,7 +1,9 @@
 //함수를 이용하여 짝수 홀수 구분하기.
 function evenOrOdd(inputNum) {
   //삼항연산자로 결과를 한줄로 출력한다.
-  return (inputNum%2 === 0)? 'even' : 'odd';
+  return (inputNum%2)? 'odd' : 'even';
 }
 //짝수 홀수 구분할 숫자를 입력한다.
 console.log(evenOrOdd(9));
+console.log(evenOrOdd(1111));
+console.log(evenOrOdd(-102310));
