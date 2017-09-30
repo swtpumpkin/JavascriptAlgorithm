1c45d2d2e95693729b8e3b2629dee9779a0cb11d
JeongwonKim <swtpumpkin90@gmail.com>
2017-09-30 09:54:01 +0900

    Add comment.

--- Exercise/twoMillionPrimeSum.js
+++ Exercise/twoMillionPrimeSum.js
@@ -5,13 +5,17 @@ var result = 0;
 while(num<2000000){
   // 소수 검증하는 변수
   var priNum = true;
+  // 소수 확인을 위한 for문
   for(var divNum = 2; divNum < num; divNum++){
+    // 자기 자긴 전에 나눈 값이 0이면 false를 반환한다.
     if(num%divNum === 0){
       priNum = false;
       break;
     }
   }
+  // priNum이 true이면 소수이다.
   if(priNum){
+    // 소수의 값을 더한다.
     result += num;
   }
   num++
