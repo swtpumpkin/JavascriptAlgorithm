16bc5a9a1ba204f4b13ce81a6e400a8ff18f7ab7
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-14 23:07:06 +0900

    Remove any unnecessary code

--- Exercise/RevPyramid.js
+++ Exercise/RevPyramid.js
@@ -4,10 +4,8 @@ var inputHeight = 5;
 var result = '';
 
 for(var num=inputHeight; num>0; num--){ //피라미드 높이의 숫자부터 1까지 내려오는 loop.
-  if(num!==inputHeight){ //첫줄에는 띄어쓰기 생략을 함.
-    for(var empty = 1; empty<=inputHeight-num; empty++){ //빈칸을 입력하는 for loop.
-      result += ' '; //띄어쓰기 입력하는 코드.
-    }
+  for(var empty = 1; empty<=inputHeight-num; empty++){ //빈칸을 입력하는 for loop.
+    result += ' '; //띄어쓰기 입력하는 코드.
   }
   for(var star = num-1; star>=-num+1; star--){ //피라미드 입력하는 for loop.
     result +='*'; //별 입력하는 코드.
